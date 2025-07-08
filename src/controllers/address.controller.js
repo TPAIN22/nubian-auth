7fd1d310eb9c3ba5077e97c560e45b1c831d4baf
TPAIN22 <mamyafreka@gmail.com>
2025-07-08 13:15:08 +0300

    refactor address controller to use user model for address operations

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -1,10 +1,12 @@
 import Address from '../models/address.model.js';
 import { getAuth } from '@clerk/express';
+import User from '../models/user.model.js';
 
 export const getAddresses = async (req, res) => {
   const { userId } = getAuth(req);
   try {
-    const addresses = await Address.find({ user: userId });
+    const user = await User.findOne({ clerkId: userId });
+    const addresses = await Address.find({ user: user._id });
     res.status(200).json(addresses);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -13,11 +15,13 @@ export const getAddresses = async (req, res) => {
 
 export const addAddress = async (req, res) => {
   const { userId } = getAuth(req);
+  
   try {
+    const user = await User.findOne({ clerkId: userId });
     if (req.body.isDefault) {
-      await Address.updateMany({ user: userId }, { isDefault: false });
+      await Address.updateMany({ user: user._id }, { isDefault: false });
     }
-    const address = await Address.create({ ...req.body, user: userId });
+    const address = await Address.create({ ...req.body, user: user._id });
     res.status(201).json(address);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -27,11 +31,14 @@ export const addAddress = async (req, res) => {
 export const updateAddress = async (req, res) => {
   const { userId } = getAuth(req);
   const { id } = req.params;
+  
+  
   try {
+    const user = await User.findOne({ clerkId: userId });
     if (req.body.isDefault) {
-      await Address.updateMany({ user: userId }, { isDefault: false });
+      await Address.updateMany({ user: user._id }, { isDefault: false });
     }
-    const address = await Address.findOneAndUpdate({ _id: id, user: userId }, req.body, { new: true });
+    const address = await Address.findOneAndUpdate({ _id: id, user: user._id }, req.body, { new: true });
     res.status(200).json(address);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -42,7 +49,9 @@ export const deleteAddress = async (req, res) => {
   const { userId } = getAuth(req);
   const { id } = req.params;
   try {
-    await Address.findOneAndDelete({ _id: id, user: userId });
+    
+    const user = await User.findOne({ clerkId: userId });
+    await Address.findOneAndDelete({ _id: id, user: user._id });
     res.status(200).json({ success: true });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -53,8 +62,10 @@ export const setDefaultAddress = async (req, res) => {
   const { userId } = getAuth(req);
   const { id } = req.params;
   try {
+    const user = await User.findOne({ clerkId: userId });
+
     await Address.updateMany({ user: userId }, { isDefault: false });
-    const address = await Address.findOneAndUpdate({ _id: id, user: userId }, { isDefault: true }, { new: true });
+    const address = await Address.findOneAndUpdate({ _id: id, user: user._id }, { isDefault: true }, { new: true });
     res.status(200).json(address);
   } catch (error) {
     res.status(500).json({ message: error.message });
